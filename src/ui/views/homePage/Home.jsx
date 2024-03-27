1e9d7b026ff202131720a9a80813bda941dce82c
Valentin <80779186+ValentinGassend@users.noreply.github.com>
2024-03-27 23:48:33 +0100

    cleaned home

--- src/ui/views/homePage/Home.jsx
+++ src/ui/views/homePage/Home.jsx
@@ -1,10 +1,4 @@
-import Loader from "../../components/Loader.jsx";
-import React, {useEffect, useRef, useState} from "react";
-import ProjectManager from "../../../managers/ProjectManager.jsx";
-import gsap from "gsap";
-import {ScrollTrigger} from "gsap/ScrollTrigger";
-import Overlay from "../../components/Overlay.jsx";
-import projectManager from "../../../managers/ProjectManager.jsx";
+import React from "react";
 
 const Home = () => {
 
