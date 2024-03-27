c88180a8c852e312f863db3932c4a28b245fc9f3
Valentin <80779186+ValentinGassend@users.noreply.github.com>
2024-03-28 00:04:27 +0100

    add about

--- src/ui/views/homePage/Home.jsx
+++ src/ui/views/homePage/Home.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Landing from "./Landing/Landing.jsx";
+import About from "./About/About.jsx";
 
 const Home = () => {
 
 
     return (<>
         <Landing/>
+        <About/>
     </>)
 }
 export default Home
