444bc637d7ccd844eb004155204ee6a23decef72
Valentin <80779186+ValentinGassend@users.noreply.github.com>
2024-04-02 14:16:26 +0200

    add html elements

--- src/ui/views/homePage/About/About.jsx
+++ src/ui/views/homePage/About/About.jsx
@@ -1,11 +1,24 @@
 import React from "react";
+import SvgManager from "../../../../managers/SvgManager.jsx";
 
 const About = () => {
 
 
 
     return (<>
+        <section className={"About"}>
+            <div className={"About-container"}>
+                <div className={"About-container-item"}>
+                    <h2 className={"About-container--item---content"}>Creative</h2>
+                    <h2 className={"About-container--item---content Before After"}>Web</h2>
+                    <h2 className={"About-container--item---content"}>Developer</h2>
+                </div>
 
+                <div className={"About-container-item"}>
+                    <h2 className={"About-container--item---content"}>Figma stroke plugin frame editor overflow horizontal arrow.</h2>
+                </div>
+            </div>
+        </section>
     </>)
 }
 export default About
