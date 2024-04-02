a2971f1123a88a559c070395de02fbc84ec9a0f7
Valentin <80779186+ValentinGassend@users.noreply.github.com>
2024-04-02 14:19:28 +0200

    add style elements

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
 import Home from "./ui/views/homePage/Home.jsx";
 
 function App() {
