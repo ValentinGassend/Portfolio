b9b8207b44ec0e05f31d8f497cd4e4b99b42e188
Valentin <80779186+ValentinGassend@users.noreply.github.com>
2024-04-03 10:21:31 +0200

    add ColorManager

---
+++ src/managers/ColorManager.jsx
@@ -0,0 +1,12 @@
+const ColorManager = {
+    $color_black: "#000000",
+    $color_white: "#ffffff",
+    $color_neutral1: "#F8F5EC",
+    $color_neutral2: "#EFE4D1",
+    $color_accent1: "#9C41F7",
+    $color_accent2: "#C26140",
+    $color_accent3: "#06BA63",
+    $color_accent4: "#FBD038"
+};
+
+export default ColorManager;
