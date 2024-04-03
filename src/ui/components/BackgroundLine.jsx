b1830119e44b039c52c5825bae331e3f7636946d
Valentin <80779186+ValentinGassend@users.noreply.github.com>
2024-04-03 10:22:02 +0200

    add html of BackgroundLine component

--- src/ui/components/BackgroundLine.jsx
+++ src/ui/components/BackgroundLine.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import ColorManager from "../../managers/ColorManager.jsx";
 
-const BackgroundLine = ({verticalCount = 3, horizontalCount = 3}) => {
+const BackgroundLine = ({colorName = ColorManager.$color_neutral2, verticalCount = 3, horizontalCount = 3}) => {
 
     return (
-        <div className={"BackgroundLine"} style={{ "--horizontalCount": horizontalCount + 1, "--verticalCount": verticalCount + 1 }}>
+        <div className={"BackgroundLine"} style={{ "--horizontalCount": horizontalCount + 1, "--verticalCount": verticalCount + 1, "--colorName": colorName}}>
             <div className={"BackgroundLine-vertical"}>
                 {Array.from({length: verticalCount + 1}, (_, index) => (
                     <span key={`vertical-${index}`} className={"BackgroundLine-vertical--item"}></span>
