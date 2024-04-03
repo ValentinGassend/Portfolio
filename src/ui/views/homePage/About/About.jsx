5c45781a68a9c0faa8b19d407e52ea10863159e3
Valentin <80779186+ValentinGassend@users.noreply.github.com>
2024-04-03 11:26:13 +0200

    using component + fixing class

--- src/ui/views/homePage/About/About.jsx
+++ src/ui/views/homePage/About/About.jsx
@@ -1,20 +1,24 @@
 import React from "react";
 import SvgManager from "../../../../managers/SvgManager.jsx";
+import BackgroundLine from "../../../components/BackgroundLine.jsx";
+import ColorManager from "../../../../managers/ColorManager.jsx";
 
 const About = () => {
 
 
 
     return (<>
-        <section className={"About"}>
+        <section className={"About Before After"}>
+            <BackgroundLine colorName={ColorManager.$color_neutral1} horizontalCount={6} verticalCount={3}/>
+
             <div className={"About-container"}>
-                <div className={"About-container-item"}>
+                <div className={"About-container--item"}>
                     <h2 className={"About-container--item---content"}>Creative</h2>
                     <h2 className={"About-container--item---content Before After"}>Web</h2>
                     <h2 className={"About-container--item---content"}>Developer</h2>
                 </div>
 
-                <div className={"About-container-item"}>
+                <div className={"About-container--item"}>
                     <h2 className={"About-container--item---content"}>Figma stroke plugin frame editor overflow horizontal arrow.</h2>
                 </div>
             </div>
