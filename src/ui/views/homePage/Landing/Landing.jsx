0e8e961a3ee3786152d137aef4c422fbaf931a99
Valentin <80779186+ValentinGassend@users.noreply.github.com>
2024-04-03 10:43:29 +0200

    add BackgroundLine to Landing

--- src/ui/views/homePage/Landing/Landing.jsx
+++ src/ui/views/homePage/Landing/Landing.jsx
@@ -1,15 +1,18 @@
 import React from "react";
 import SvgManager from "../../../../managers/SvgManager.jsx";
+import BackgroundLine from "../../../components/BackgroundLine.jsx";
+import ColorManager from "../../../../managers/ColorManager.jsx";
 
 const Landing = () => {
 
 
     return (
-        <section className={"Landing"}>
+        <section className={"Landing Center After"}>
+            <BackgroundLine colorName={ColorManager.$color_neutral2}/>
             <div className={"Landing-background"}>
                 <SvgManager name={"n_element"} parentClassName={"Landing-background"}/>
             </div>
-            <div className={"Landing-container"}>
+            <div className={"Landing-container Center"}>
                 <SvgManager name={"full_logo"} parentClassName={"Landing-container"}/>
             </div>
         </section>
