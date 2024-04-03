8e2d8aa99222797961cb77829ab663fc94c76579
Valentin <80779186+ValentinGassend@users.noreply.github.com>
2024-04-03 10:11:18 +0200

    add html of BackgroundLine component

---
+++ src/ui/components/BackgroundLine.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const BackgroundLine = ({verticalCount = 3, horizontalCount = 3}) => {
+
+    return (
+        <div className={"BackgroundLine"} style={{ "--horizontalCount": horizontalCount + 1, "--verticalCount": verticalCount + 1 }}>
+            <div className={"BackgroundLine-vertical"}>
+                {Array.from({length: verticalCount + 1}, (_, index) => (
+                    <span key={`vertical-${index}`} className={"BackgroundLine-vertical--item"}></span>
+                ))}
+            </div>
+
+            <div className={"BackgroundLine-horizontal"}>
+                {Array.from({length: horizontalCount + 1}, (_, index) => (
+                    <span key={`horizontal-${index}`} className={"BackgroundLine-horizontal--item"}></span>
+                ))}
+            </div>
+        </div>
+    );
+};
+
+export default BackgroundLine;
