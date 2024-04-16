f56554b8f681d4eadce1f823a1ff1f2b8681ec79
Valentin <80779186+ValentinGassend@users.noreply.github.com>
2024-04-16 17:18:18 +0200

    modifying Contact masking

--- src/ui/views/homePage/Contact/components/Contact_masking.jsx
+++ src/ui/views/homePage/Contact/components/Contact_masking.jsx
@@ -1,20 +1,30 @@
 import React from "react";
 import SvgManager from "../../../../../managers/SvgManager.jsx";
 import ContactMaskingBackground from "./Contact_masking_background.jsx";
+import BackgroundLine from "../../../../components/BackgroundLine.jsx";
 
 const ContactMasking = () => {
 
 
     return (<>
         <div className={"ContactMasking"}>
+            <ContactMaskingBackground/>
+
             <div className="ContactMasking-frontground">
                 <span className="ContactMasking-frontground--content Center Uppercase">Let's contact !</span>
             </div>
 
             <div className="ContactMasking-cursor __ContactMaskingCursor">
                 <SvgManager name={`ContactMaskingCursor`} parentClassName={`ContactMasking-cursor`}/>
-                <ContactMaskingBackground/>
+
+                <div className="ContactMasking-background">
+                    <span className="ContactMasking-background--content Center Uppercase">Let's contact !</span>
+                    <BackgroundLine/>
+
+                </div>
+
             </div>
+
         </div>
     </>)
 }
