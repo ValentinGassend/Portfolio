402d63198c3d2945ecc79cb7555b6d66492bbf83
Valentin <80779186+ValentinGassend@users.noreply.github.com>
2024-04-17 21:49:09 +0200

    scroll animation

--- src/ui/views/homePage/About/About.jsx
+++ src/ui/views/homePage/About/About.jsx
@@ -1,25 +1,52 @@
-import React from "react";
+import React, {useEffect, useLayoutEffect} from "react";
 import SvgManager from "../../../../managers/SvgManager.jsx";
 import BackgroundLine from "../../../components/BackgroundLine.jsx";
 import ColorManager from "../../../../managers/ColorManager.jsx";
+import gsap from "gsap";
+import {ScrollTrigger} from "gsap/ScrollTrigger";
 
 const About = () => {
+    gsap.registerPlugin(ScrollTrigger);
+
+    useEffect(() => {
+        gsap.to(".__AboutSticky", {
+            immediateRender: true, overwrite:true, opacity: 0.06, filter: "blur(8px)",
+            scrollTrigger: {
+                trigger: ".__AboutSticky",
+                scrub: true,
+                start: "top top",
+                end: "bottom top",
+                pin:".__AboutSticky",
+            },
+        });
+        gsap.to(".__AboutToProjectSticky", {
+            immediateRender: true, overwrite: true,
+            scrollTrigger: {
+                trigger: ".__AboutToProjectSticky",
+                scrub: true,
+                start: "top top",
+                end: "bottom top",
+                pin: ".__AboutToProjectSticky",
+            },
+        });
 
 
+    }, []);
 
     return (<>
         <section className={"About Before After"}>
             <BackgroundLine colorName={ColorManager.$color_neutral1} horizontalCount={6} verticalCount={3}/>
 
             <div className={"About-container"}>
-                <div className={"About-container--item"}>
+                <div className={"About-container--item __AboutSticky"}>
                     <h2 className={"About-container--item---content Uppercase"}>Creative</h2>
                     <h2 className={"About-container--item---content Uppercase Before After"}>Web</h2>
                     <h2 className={"About-container--item---content Uppercase"}>Developer</h2>
                 </div>
 
-                <div className={"About-container--item"}>
-                    <h2 className={"About-container--item---content Uppercase TxtCenter"}>Figma stroke plugin frame editor overflow horizontal arrow.</h2>
+                <div className={"About-container--item __AboutToProjectSticky"}>
+                    <h2 className={"About-container--item---content Uppercase TxtCenter"}>Figma stroke plugin frame
+                        editor overflow horizontal arrow.</h2>
                 </div>
             </div>
         </section>
