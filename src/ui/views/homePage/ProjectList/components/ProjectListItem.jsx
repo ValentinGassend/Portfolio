adf105b304f6aa8d54502a9633f8dcdea580e163
Valentin <80779186+ValentinGassend@users.noreply.github.com>
2024-04-17 21:51:06 +0200

    image appear on hover

--- src/ui/views/homePage/ProjectList/components/ProjectListItem.jsx
+++ src/ui/views/homePage/ProjectList/components/ProjectListItem.jsx
@@ -1,10 +1,9 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 
-const ProjectListItem = ({project}) => {
-
+const ProjectListItem = ({project, mousePosition}) => {
 
     return (<>
-        <div className={"ProjectListItem-header"} >
+        <div className={"ProjectListItem-header"}>
             {project.date} - {project.client}
 
         </div>
@@ -12,13 +11,21 @@ const ProjectListItem = ({project}) => {
             {project.name}
         </h3>
         <div className={"ProjectListItem-subtitle"}>
-        {project.tags.map((tag, index) => (
+            {project.tags.map((tag, index) => (
                 <p key={index} className={"ProjectListItem-subtitle--tag Before"}>
                     {tag}
                 </p>
-        ))}
+            ))}
         </div>
-        <img className={"ProjectListItem-img"} src={project.imageUrl} alt={project.name}/>
+        <img
+            className={"ProjectListItem-img"}
+            src={project.imageUrl}
+            alt={project.name}
+            style={{
+                left: mousePosition.x,
+                top: mousePosition.y,
+            }}
+        />
     </>)
 }
 export default ProjectListItem
