8a3e349e88ea6ab3c202496b8d79dc8485577cb6
Valentin <80779186+ValentinGassend@users.noreply.github.com>
2024-04-18 16:01:32 +0200

    add smooth scroll

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,33 @@
-import React, {useState} from 'react'
+import React, {useEffect, useState} from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import Home from "./ui/views/homePage/Home.jsx";
 import Menu from "./ui/components/menu/Menu.jsx";
+import gsap from "gsap";
+import {ScrollSmoother} from "gsap/ScrollSmoother";
+import {ScrollTrigger} from "gsap/ScrollTrigger";
+import CustomEase from "gsap/CustomEase";
 
 function App() {
-    const [count, setCount] = useState(0)
+    gsap.registerPlugin(ScrollTrigger, ScrollSmoother, CustomEase);
+    CustomEase.create("hop", ".235, .615, .185, .995")
+    useEffect(() => {
+        if (document.getElementsByClassName("__ScrollSmooth")) {
 
-    return (
-        <>
-            <Home/>
-            <Menu/>
-        </>
-    )
+            ScrollSmoother.create({
+                content: ".__ScrollSmooth",
+                smooth: 0.5,
+                effects:true,
+                speed:1.2,
+                ease: "hop"
+            });
+        }
+
+    }, []);
+    return (<div className={`__ScrollSmooth`}>
+        <Home/>
+        <Menu/>
+    </div>)
 }
 
 export default App
