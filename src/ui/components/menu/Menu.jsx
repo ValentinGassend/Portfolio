68e89f684121f3b626a784d2130eebab54d0b373
Valentin <80779186+ValentinGassend@users.noreply.github.com>
2024-04-18 14:46:59 +0200

    add close menu functionality

--- src/ui/components/menu/Menu.jsx
+++ src/ui/components/menu/Menu.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import MenuSection from "./components/MenuSection.jsx";
 import {Swiper} from "swiper/react";
 import {SwiperSlide} from "swiper/react";
@@ -10,7 +10,27 @@ import {Autoplay} from "swiper/modules";
 const Menu = () => {
 
 
-    return (<div className={"Menu"}>
+    const [menu, setMenu] = useState(null);
+
+    useEffect(() => {
+        const menuElement = document.querySelector('.Menu');
+        setMenu(menuElement);
+    }, []);
+
+    const closeMenu = (evt) => {
+        if (menu && menu.classList.contains("active")) {
+            menu.classList.remove("active");
+        }
+    };
+    useEffect(() => {
+        if (menu) {
+            menu.addEventListener("click", closeMenu);
+            return () => {
+                menu.removeEventListener("click", closeMenu);
+            };
+        }
+    }, [menu]);
+    return (<div className={"Menu active"}>
         <Swiper
             slidesPerView={'auto'}
             autoplay={{
@@ -39,10 +59,12 @@ const Menu = () => {
                 <MenuSection/>
             </SwiperSlide>
         </Swiper>
-        <svg xmlns="http://www.w3.org/2000/svg" className={`Menu-close`} width="37" height="37" viewBox="0 0 37 37" fill="none">
+        <div className={`Menu-close`}>
+        <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" viewBox="0 0 37 37" fill="none">
             <path d="M4.20458 -0.000234323L0 4.20435L32.7958 37.0001L37.0003 32.7955L4.20458 -0.000234323Z" fill="black"/>
             <path d="M5.47257e-06 32.7955L4.20459 37.0001L37.0003 4.20434L32.7958 -0.000244141L5.47257e-06 32.7955Z" fill="black"/>
         </svg>
+        </div>
     </div>)
 }
 export default Menu
