50074673ad7da1092422e497e6c03b5110ce9d97
Valentin <80779186+ValentinGassend@users.noreply.github.com>
2024-04-18 14:20:33 +0200

    add menu to page

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
-import { useState } from 'react'
+import React, {useState} from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import Home from "./ui/views/homePage/Home.jsx";
+import Menu from "./ui/components/menu/Menu.jsx";
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <Home/>
-    </>
-  )
+    return (
+        <>
+            <Home/>
+            <Menu/>
+        </>
+    )
 }
 
 export default App
