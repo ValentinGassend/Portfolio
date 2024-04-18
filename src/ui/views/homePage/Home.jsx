08f51609b871ffa986cbf6490fcbf5fd94f7653e
Valentin <80779186+ValentinGassend@users.noreply.github.com>
2024-04-18 15:57:44 +0200

    add overlay

--- src/ui/views/homePage/Home.jsx
+++ src/ui/views/homePage/Home.jsx
@@ -3,6 +3,7 @@ import Landing from "./Landing/Landing.jsx";
 import About from "./About/About.jsx";
 import ProjectList from "./ProjectList/ProjectList.jsx";
 import Contact from "./Contact/Contact.jsx";
+import Overlay from "../../components/Overlay.jsx";
 
 const Home = () => {
 
@@ -12,6 +13,7 @@ const Home = () => {
         <About/>
         <ProjectList/>
         <Contact/>
+        <Overlay/>
     </section>)
 }
 export default Home
