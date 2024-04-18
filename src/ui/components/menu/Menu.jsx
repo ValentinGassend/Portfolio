0a2c1c44efc98feb3c570fd0d99205fdeb29e1da
Valentin <80779186+ValentinGassend@users.noreply.github.com>
2024-04-18 15:59:48 +0200

    fix menu trigger

--- src/ui/components/menu/Menu.jsx
+++ src/ui/components/menu/Menu.jsx
@@ -24,13 +24,13 @@ const Menu = () => {
     };
     useEffect(() => {
         if (menu) {
-            menu.addEventListener("click", closeMenu);
+            document.querySelector('.__MenuCloser').addEventListener("click", closeMenu);
             return () => {
-                menu.removeEventListener("click", closeMenu);
+                document.querySelector('.__MenuCloser').removeEventListener("click", closeMenu);
             };
         }
     }, [menu]);
-    return (<div className={"Menu active"}>
+    return (<div className={"Menu"}>
         <Swiper
             slidesPerView={'auto'}
             autoplay={{
@@ -59,7 +59,7 @@ const Menu = () => {
                 <MenuSection/>
             </SwiperSlide>
         </Swiper>
-        <div className={`Menu-close`}>
+        <div className={`Menu-close __MenuCloser`}>
         <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" viewBox="0 0 37 37" fill="none">
             <path d="M4.20458 -0.000234323L0 4.20435L32.7958 37.0001L37.0003 32.7955L4.20458 -0.000234323Z" fill="black"/>
             <path d="M5.47257e-06 32.7955L4.20459 37.0001L37.0003 4.20434L32.7958 -0.000244141L5.47257e-06 32.7955Z" fill="black"/>
