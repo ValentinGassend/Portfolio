88b1dd2f4d4e57f1bf3bbb67728b2d7694da61d5
Valentin <80779186+ValentinGassend@users.noreply.github.com>
2024-04-25 00:28:06 +0200

    add projectsPage overlay html modification

--- src/ui/components/Overlay.jsx
+++ src/ui/components/Overlay.jsx
@@ -1,14 +1,46 @@
-import React, {useEffect, useState} from "react";
+import React, {useEffect, useRef, useState} from "react";
+import ProjectSingle from "../views/projectsPage/projectSingle/projectSingle.jsx";
+import gsap from "gsap";
+import ScrollToPlugin from "gsap/ScrollToPlugin";
 
-const Overlay = () => {
+const Overlay = ({projectsPage = false, projectsList}) => {
 
     const [menu, setMenu] = useState(null);
-
+    gsap.registerPlugin(ScrollToPlugin)
     useEffect(() => {
-        const menuElement = document.querySelector('.Menu');
+        const menuElement = document.querySelector(".Menu");
         setMenu(menuElement);
+
     }, []);
 
+    useEffect(() => {
+        const projectsList = document.querySelectorAll(".Overlay-center--item---text");
+        projectsList.forEach((projectItem, index) => {
+            projectItem.addEventListener('click', function (evt) {
+                evt.preventDefault();
+                let segments = evt.target.href.split("#");
+                let lastSegment = segments[segments.length - 1];
+                const targetElement = document.querySelector(`#${lastSegment}`);
+                console.log(targetElement)
+                scrollToTargetElement(targetElement);
+            });
+        });
+    }, []);
+    const scrollToTargetElement = (targetElement) => {
+        if (targetElement) {
+            const offset = targetElement.getBoundingClientRect().top + window.scrollY;
+
+            gsap.to(window, {
+                duration: 1.2,
+                scrollTo: {
+                    y: offset,
+                    smooth: 0.5,
+                    behavior:"smooth"
+                },
+                ease: "hop"
+            })
+        }
+    };
     const openMenu = (evt) => {
         if (menu && !menu.classList.contains("active")) {
             menu.classList.add("active");
@@ -16,39 +48,69 @@ const Overlay = () => {
     };
     useEffect(() => {
         if (menu) {
-            document.querySelector('.__MenuOpener').addEventListener("click", openMenu);
+            document
+                .querySelector(".__MenuOpener")
+                .addEventListener("click", openMenu);
             return () => {
-                document.querySelector('.__MenuOpener').removeEventListener("click", openMenu);
+                document
+                    .querySelector(".__MenuOpener")
+                    .removeEventListener("click", openMenu);
             };
         }
     }, [menu]);
     return (<>
-        <div className={`Overlay`}>
-            <div className={`Overlay-upper`}>
-                <div className={`Overlay-upper--item`}>
-                    <p className={`Overlay-upper--item---text Uppercase`}>Creative web developer</p>
+        {projectsPage && projectsList ? <div className={`Overlay Projects`}>
+                <div className={`Overlay-upper`}>
+                    <div className={`Overlay-upper--item`}>
+                        <p className={`Overlay-upper--item---text Uppercase`}>Creative web developer</p>
+                    </div>
+                    <div className={`Overlay-upper--item __MenuOpener`}>
+                        <p className={`Overlay-upper--item---text Uppercase`}>ME<br/>NU</p>
+                    </div>
                 </div>
-                <div className={`Overlay-upper--item`}>
-                    <p className={`Overlay-upper--item---text Uppercase`}>Based in annecy</p>
+                <div className={`Overlay-center`}>
+                    <div className={`Overlay-center--item `}>
+                        {projectsList.map((project, index) => (<a key={index} href={`#project${index}`}
+                                                                  className={`Overlay-center--item---text Uppercase Before`}>
+                            {project.title}
+                        </a>))}
+                    </div>
                 </div>
-                <div className={`Overlay-upper--item __MenuOpener`}>
-                    <p className={`Overlay-upper--item---text Uppercase`}>ME<br/>NU</p>
-                </div>
-            </div>
-            <div className={`Overlay-lower`}>
+                <div className={`Overlay-lower`}>
 
-                <div className={`Overlay-lower--item`}>
-                    <p className={`Overlay-lower--item---text Uppercase`}>Open to work</p>
+                    <div className={`Overlay-lower--item`}>
+                        <p className={`Overlay-lower--item---text Uppercase`}>Projects</p>
+                    </div>
                 </div>
-                <div className={`Overlay-lower--item`}>
-                    <p className={`Overlay-lower--item---text Uppercase`}>Portfolio</p>
-                </div>
-                <div className={`Overlay-lower--item`}>
-                    <p className={`Overlay-lower--item---text Uppercase`}>2024</p>
-                </div>
+
             </div>
 
-        </div>
+            : <div className={`Overlay`}>
+                <div className={`Overlay-upper`}>
+                    <div className={`Overlay-upper--item`}>
+                        <p className={`Overlay-upper--item---text Uppercase`}>Creative web developer</p>
+                    </div>
+                    <div className={`Overlay-upper--item`}>
+                        <p className={`Overlay-upper--item---text Uppercase`}>Based in annecy</p>
+                    </div>
+                    <div className={`Overlay-upper--item __MenuOpener`}>
+                        <p className={`Overlay-upper--item---text Uppercase`}>ME<br/>NU</p>
+                    </div>
+                </div>
+                <div className={`Overlay-lower`}>
+
+                    <div className={`Overlay-lower--item`}>
+                        <p className={`Overlay-lower--item---text Uppercase`}>Open to work</p>
+                    </div>
+                    <div className={`Overlay-lower--item`}>
+                        <p className={`Overlay-lower--item---text Uppercase`}>Portfolio</p>
+                    </div>
+                    <div className={`Overlay-lower--item`}>
+                        <p className={`Overlay-lower--item---text Uppercase`}>2024</p>
+                    </div>
+                </div>
+
+            </div>}
     </>)
 }
 export default Overlay
