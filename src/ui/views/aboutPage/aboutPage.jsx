336dc8220d315efff7629dccbef3b1976f0351df
Valentin <80779186+ValentinGassend@users.noreply.github.com>
2024-04-27 15:59:04 +0200

    about html modification

--- src/ui/views/aboutPage/aboutPage.jsx
+++ src/ui/views/aboutPage/aboutPage.jsx
@@ -5,13 +5,25 @@ import {ScrollTrigger} from "gsap/ScrollTrigger";
 import AboutLanding from "./Landing/AboutLanding.jsx";
 import AboutIntro from "./Intro/AboutIntro.jsx";
 import AboutDetails from "./Details/AboutDetails.jsx";
+import {Autoplay, FreeMode, Mousewheel} from "swiper/modules";
+import {Swiper, SwiperSlide} from "swiper/react";
 
 const AboutPage = () => {
 
 
     gsap.registerPlugin(ScrollTrigger)
+    let sections = gsap.utils.toArray(".__HorizontalPanel");
     useEffect(() => {
-
+        // gsap.to(sections, {
+        //     xPercent: -100 * (sections.length - 1), ease: "none", scrollTrigger: {
+        //         trigger: ".__HorizontalScrollContainer",
+        //         pin: ".__HorizontalScrollContainer",
+        //         scrub: true,
+        //         markers: true,
+        //         snap: 1 / (sections.length - 1),
+        //         end: () => "+=" + document.querySelector(".__HorizontalScrollContainer").offsetWidth
+        //     }
+        // });
     }, []);
 
 
@@ -20,8 +32,27 @@ const AboutPage = () => {
         <div className={`__ScrollSmooth`}>
             <section className={"About"}>
                 <AboutLanding/>
-                <AboutIntro/>
-                <AboutDetails/>
+                <Swiper
+                    slidesPerView={'auto'}
+                    // loop={true}
+                    freeMode={true}
+                    modules={[Mousewheel, FreeMode]}
+                    mousewheel={{
+                        enabled: true,
+                        releaseOnEdges: true,
+                        eventsTarget:".About-swiper",
+                        sensitivity:7.5
+                    }}
+                className={`About-swiper`}
+                >
+                    <SwiperSlide>
+                        <AboutIntro/>
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <AboutDetails/>
+                    </SwiperSlide>
+
+                </Swiper>
             </section>
         </div>
     </>);
