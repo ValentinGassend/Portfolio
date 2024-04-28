ca30074bf3fd3f7f606b1c1483028e65c4c753fd
Valentin <80779186+ValentinGassend@users.noreply.github.com>
2024-04-28 22:56:15 +0200

    modify menu

--- src/ui/components/menu/components/MenuSection.jsx
+++ src/ui/components/menu/components/MenuSection.jsx
@@ -6,38 +6,37 @@ const MenuSection = () => {
     return (
         <div className={"MenuSection"}>
             <div className={"MenuSection-item"}>
-                <p className={"MenuSection-item--text Uppercase"}>Creative web developer</p>
-                <p className={"MenuSection-item--text Uppercase"}>Based in annecy</p>
+                <p className={"MenuSection-item--text Uppercase"}>Développeur web créatif</p>
+                <p className={"MenuSection-item--text Uppercase"}>Basé à annecy</p>
             </div>
+            <a href="/" className={"MenuSection-item"}>
+                <h1 className={"MenuSection-item--title Uppercase"}>
+
+                    Accueil
+                </h1>
+
+            </a>
             <a href="/projects" className={"MenuSection-item"}>
                 <h1 className={"MenuSection-item--title Uppercase"}>
-                    Projects
+                    Projets
 
                 </h1>
             </a>
             <a href="/about" className={"MenuSection-item"}>
                 <h1 className={"MenuSection-item--title Uppercase"}>
 
-                    About
+                    à propos
                 </h1>
 
             </a>
             <a href="#" className={"MenuSection-item"}>
                 <h1 className={"MenuSection-item--title Uppercase"}>
-
-                    Contact
-                </h1>
-
-            </a>
-            <a href="#" className={"MenuSection-item"}>
-                <h1 className={"MenuSection-item--title Uppercase"}>
-
-                    The lab
+                    Labo
                 </h1>
 
             </a>
             <div className={"MenuSection-item"}>
-                <p className={"MenuSection-item--text Uppercase"}>Open to work</p>
+                <p className={"MenuSection-item--text Uppercase"}>En recherche d'alternance</p>
                 <p className={"MenuSection-item--text Uppercase"}>Portfolio</p>
             </div>
         </div>
