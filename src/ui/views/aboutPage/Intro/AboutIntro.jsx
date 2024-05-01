d90dd94745e22ff0f9743c4062ad23e1b431412d
Valentin <80779186+ValentinGassend@users.noreply.github.com>
2024-05-02 00:04:41 +0200

    fixs

--- src/ui/views/aboutPage/Intro/AboutIntro.jsx
+++ src/ui/views/aboutPage/Intro/AboutIntro.jsx
@@ -18,10 +18,14 @@ const AboutIntro = () => {
                 <div className={"AboutIntro-container--item After"}>
                     <img src={`https://picsum.photos/1800/1800`}
                          className={`AboutIntro-container--item---background `} alt={""}/>
-                    <p className={"AboutIntro-container--item---content"}> Cursus tellus sit faucibus fermentum. Mattis
-                        euismod consequat commodo potenti tortor cursus congue
-                        id. Dolor commodo pulvinar nullam volutpat nisl erat. Dui commodo scelerisque hendrerit iaculis
-                        nulla. Eu lobortis lacus et ultrices. Consectetur.</p>
+                    <p className={"AboutIntro-container--item---content"}> Je suis Valentin Gassend,étudiant et
+                        développeur
+                        passionné de 22 ans. J'ai exploré le développement
+                        dans divers domaines pendant ces sept dernières années, commençant par le développement web
+                        avant de m'aventurer dans l'IoT, le mobile, les jeux vidéos, et bien plus encore. Cependant,
+                        c'est dans le développement web que se trouve ma véritable passion. ce portfolio est un chouette
+                        reflet de mes réalisations aussi bien professionnelles que personnelles retracé avec une touche
+                        de personnalité.</p>
                 </div>
             </div>
         </div>);
