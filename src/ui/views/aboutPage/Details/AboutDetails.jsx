f973be103a7a71730040ebb77a999c2090d2c5ca
Valentin <80779186+ValentinGassend@users.noreply.github.com>
2024-05-02 00:04:34 +0200

    fixs

--- src/ui/views/aboutPage/Details/AboutDetails.jsx
+++ src/ui/views/aboutPage/Details/AboutDetails.jsx
@@ -23,27 +23,28 @@ const AboutDetails = () => {
             <div className={"AboutDetails-container"}>
 
                 <div className={"AboutDetails-container--item"}>
-                    <p className={"AboutDetails-container--item---content"}> Cursus tellus sit faucibus fermentum.
-                        Mattis
-                        euismod consequat commodo potenti tortor cursus congue
-                        Amet pulvinar tincidunt cursus sit cursus libero. Lorem integer diam metus non sit. Vulputate
-                        quis
-                        blandit tempor turpis quis ullamcorper. Tincidunt pretium ornare ante aliquet facilisi diam. A
-                        euismod aenean eget malesuada quam vitae mus tempus vitae.</p>
-                    <p className={"AboutDetails-container--item---content"}> Cursus tellus sit faucibus fermentum.
-                        Mattis
-                        euismod consequat commodo potenti tortor cursus congue
-                        Amet pulvinar tincidunt cursus sit cursus libero. Lorem integer diam metus non sit. Vulputate
-                        quis
-                        blandit tempor turpis quis ullamcorper. Tincidunt pretium ornare ante aliquet facilisi diam. A
-                        euismod aenean eget malesuada quam vitae mus tempus vitae.</p>
+                    <p className={"AboutDetails-container--item---content"}> Mon parcours de développement a commencé il
+                        y a sept ans avec la création de sites web statiques en HTML et CSS. Au fil du temps, j'ai
+                        élargi mes compétences pour inclure le développement web dynamique avec JavaScript, PHP et des
+                        frameworks comme Blade pour Laravel. J'ai ensuite exploré le domaine des applications web, en
+                        utilisant des technologies modernes telles que React, Vue.js et Three.js pour créer des
+                        expériences utilisateur innovantes. En parallèle, j'ai également exploré l'IoT, le développement
+                        mobile et même le jeu vidéo, élargissant ainsi mes horizons et enrichissant mes connaissances
+                        dans le domaine de la technologie.</p>
+                    <p className={"AboutDetails-container--item---content"}> Au cours de mes deux dernières années en
+                        alternance, j'ai eu l'occasion d'enrichir mes compétences en développement front dans le monde
+                        professionnel. De la mise en place de CMS Wordpress à la création de sites interactifs avec
+                        React, ainsi que des expériences 3D avec ThreeJS, j'ai pu approfondir mes connaissances et
+                        acquérir une expérience solide dans ce domaine en constante évolution. En parallèle, mon
+                        parcours à l'école a été une source d'inspiration pour l'innovation. Les projets de groupe
+                        stimulants, les séances de brainstorming mêlant développeurs et designers passionnés, ainsi que
+                        les défis techniques, ont nourri ma créativité, me poussant à repousser les limites de mes
+                        compétences techniques.</p>
                 </div>
 
                 <div className={"AboutDetails-container--item"}>
                     <a href={`/`} className={"AboutDetails-container--item---content Uppercase"}> Retour
-                        sur
-                        la
-                        home</a>
+                        a l'accueil</a>
                 </div>
             </div>
 
