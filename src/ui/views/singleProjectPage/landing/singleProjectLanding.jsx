2b69eec20a6170fcc0e6e3c4edc96df603293b57
Valentin <80779186+ValentinGassend@users.noreply.github.com>
2024-05-01 13:36:12 +0200

    responsive part & fixs

--- src/ui/views/singleProjectPage/landing/singleProjectLanding.jsx
+++ src/ui/views/singleProjectPage/landing/singleProjectLanding.jsx
@@ -16,32 +16,37 @@ const SingleProjectLanding = ({project}) => {
         {/*<div className={`__ScrollSmooth`}>*/}
 
         <section className={"SingleProjectLanding __SingleProjectSection"}>
-            <div className={"SingleProjectLanding-headline"}>
-                <div className={"SingleProjectLanding-headline--header"}>
-                    <h3 className={"SingleProjectLanding-headline--header---item Uppercase"}>
-                        {project.year}
-                    </h3>
-                    <h3 className={"SingleProjectLanding-headline--header---item Uppercase"}>
-                        {project.client}
-                    </h3>
+            <div className={"SingleProjectLanding-wrapper"}>
+                <div className={"SingleProjectLanding-headline"}>
+                    <div className={"SingleProjectLanding-headline--header"}>
+                        <h3 className={"SingleProjectLanding-headline--header---item Uppercase"}>
+                            {project.year}
+                        </h3>
+                        <h3 className={"SingleProjectLanding-headline--header---item Uppercase"}>
+                            {project.client}
+                        </h3>
+                    </div>
+                    <h3 className={"SingleProjectLanding-headline--title Uppercase"}>{project.title}</h3>
+
+
+                    <div className={"SingleProjectLanding-headline--subtitle"}>
+                        {project.tags.map((tag, index) => (
+                            <p key={index} className={"SingleProjectLanding-headline--subtitle---tag Uppercase"}>
+                                {tag}
+                            </p>))}
+                    </div>
                 </div>
-                <h3 className={"SingleProjectLanding-headline--title Uppercase"}>{project.title}</h3>
-
-
-                <div className={"SingleProjectLanding-headline--subtitle"}>
-                    {project.tags.map((tag, index) => (<p key={index} className={"SingleProjectLanding-headline--subtitle---tag Uppercase"}>
-                        {tag}
-                    </p>))}
+                <div className={"SingleProjectLanding-content"}>
+                    <p
+                        className={"SingleProjectLanding-content--text"}
+                        dangerouslySetInnerHTML={{__html: project.content.description.text.replace(/\n/g, "<br>")}}
+                    />
                 </div>
             </div>
-            <div className={"SingleProjectLanding-content"}>
-                <p
-                    className={"SingleProjectLanding-content--text"}
-                    dangerouslySetInnerHTML={{ __html: project.content.description.replace(/\n/g, "<br>") }}
-                />
-            </div>
-
+            <img src={project.content.description.image}
+                 className={`SingleProjectLanding-image`} alt={""}/>
         </section>
+
         {/*</div>*/}
 
     </>);
