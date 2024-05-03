d91da0f9d3106c9be20d785a06e6a2c49ea3b7a2
Valentin <80779186+ValentinGassend@users.noreply.github.com>
2024-05-03 02:48:48 +0200

    add scroll effect

--- src/ui/views/aboutPage/Intro/AboutIntro.jsx
+++ src/ui/views/aboutPage/Intro/AboutIntro.jsx
@@ -1,24 +1,44 @@
 import React, {useEffect} from "react";
 import gsap from "gsap";
 import {ScrollTrigger} from "gsap/ScrollTrigger";
+import {SplitText} from "gsap/SplitText";
+import {IsMobile} from "../../../../utils/utils.jsx";
 
 const AboutIntro = () => {
 
 
-    gsap.registerPlugin(ScrollTrigger)
+    gsap.registerPlugin(ScrollTrigger, SplitText)
     useEffect(() => {
+        if (!IsMobile()) {
+            let AboutTextAppearSplit
 
+            if (document.getElementsByClassName("__AboutTextAppear") && !document.getElementsByClassName("__AboutTextAppear-line")[0]) {
+                AboutTextAppearSplit = new SplitText(".__AboutTextAppear", {
+                    type: "lines", linesClass: "__AboutTextAppear-line"
+                })
+            }
+            if (AboutTextAppearSplit) {
+                gsap.to(AboutTextAppearSplit.lines, {
+                    backgroundPositionX: "0%", stagger: 0.2, scrollTrigger: {
+                        trigger: ".__AboutTextAppearContainer", scrub: 1,
+
+                        start: "top center", end: "center center"
+                    }
+                });
+            }
+        }
     }, []);
 
 
     return (
 
         <div className={"AboutIntro __HorizontalPanel"}>
-            <div className={"AboutIntro-container"}>
+            <div className={"AboutIntro-container __AboutTextAppearContainer"}>
                 <div className={"AboutIntro-container--item After"}>
                     <img src={`https://picsum.photos/1800/1800`}
                          className={`AboutIntro-container--item---background `} alt={""}/>
-                    <p className={"AboutIntro-container--item---content"}> Je suis Valentin Gassend,étudiant et
+                    <p className={"AboutIntro-container--item---content __AboutTextAppear"}> Je suis Valentin
+                        Gassend,étudiant et
                         développeur
                         passionné de 22 ans. J'ai exploré le développement
                         dans divers domaines pendant ces sept dernières années, commençant par le développement web
