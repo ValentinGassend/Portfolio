ac3d2b536bb769a38430f070849dea4a5cbed4dc
Valentin <80779186+ValentinGassend@users.noreply.github.com>
2024-07-23 12:41:30 +0200

    remove unused webgl canvas

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,19 @@
-import React, {useEffect, useState} from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React, {useEffect} from 'react'
 import Home from "./ui/views/homePage/Home.jsx";
-import Menu from "./ui/components/menu/Menu.jsx";
 import gsap from "gsap";
 import {ScrollSmoother} from "gsap/ScrollSmoother";
 import {ScrollTrigger} from "gsap/ScrollTrigger";
 import CustomEase from "gsap/CustomEase";
-import {BrowserRouter, createBrowserRouter, Route, Router, RouterProvider, Routes, useLocation} from "react-router-dom";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 import Projects from "./ui/views/projectsPage/projects.jsx";
-import About from "./ui/views/aboutPage/aboutPage.jsx";
 import AboutPage from "./ui/views/aboutPage/aboutPage.jsx";
 import SingleProjectPage from "./ui/views/singleProjectPage/singleProjectPage.jsx";
 import Lenis from "lenis";
-import Functionality3d from "./ui/views/fonctionality/Functionality3d.jsx";
+import {WebglCanvasRemover} from "./utils/utils.jsx";
 
 function App() {
+
+    WebglCanvasRemover()
     gsap.registerPlugin(ScrollTrigger, ScrollSmoother, CustomEase);
     CustomEase.create("hop", ".235, .615, .185, .995")
     useEffect(() => {
@@ -34,15 +32,14 @@ function App() {
 
     }, []);
     return (<>
-            <BrowserRouter>
-                <Routes>
-                    <Route path={"/"} element={<Home/>}/>
-                    <Route path={"/projects"} element={<Projects/>}/>
-                    <Route path={"/about"} element={<AboutPage/>}/>
-                    <Route path={"/func3d"} element={<Functionality3d/>}/>
-                    <Route path={"/project/:id"}  element={<SingleProjectPage/>}/>
-                </Routes>
-            </BrowserRouter>
+        <BrowserRouter>
+            <Routes>
+                <Route path={"/"} element={<Home/>}/>
+                <Route path={"/projects"} element={<Projects/>}/>
+                <Route path={"/about"} element={<AboutPage/>}/>
+                <Route path={"/project/:id"} element={<SingleProjectPage/>}/>
+            </Routes>
+        </BrowserRouter>
     </>)
 }
 
