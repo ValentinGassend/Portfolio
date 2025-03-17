d355889e6db7fd3f73958da01042754b7cd69b70
Valentin <80779186+ValentinGassend@users.noreply.github.com>
2025-03-17 17:43:27 +0100

    fix: optimise

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import {lazy, useEffect} from 'react'
+import {lazy, Suspense, useEffect} from 'react'
 // import Home from "./ui/views/homePage/Home.jsx";
 import gsap from "gsap";
 import {ScrollSmoother} from "gsap/ScrollSmoother";
@@ -13,11 +13,17 @@ import {WebglCanvasRemover} from "./utils/utils.jsx";
 import {Helmet} from "react-helmet";
 
 
-
 const Home = lazy(() => import('./ui/views/homePage/Home.jsx'));
 const Projects = lazy(() => import('./ui/views/projectsPage/projects.jsx'));
 const AboutPage = lazy(() => import('./ui/views/aboutPage/aboutPage.jsx'));
 const SingleProjectPage = lazy(() => import('./ui/views/singleProjectPage/singleProjectPage.jsx'));
+
+
+const LoadingSpinner = () => (
+    <div className="loading-spinner">
+        <div className="spinner"></div>
+    </div>
+);
 
 function App() {
 
@@ -37,27 +43,33 @@ function App() {
         }
 
         requestAnimationFrame(raf)
-
+        return () => {
+            // Nettoyage si nécessaire
+        };
     }, []);
     return (
         <>
             <Helmet>
-                <meta charSet="UTF-8" />
-                <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-                <meta name="author" content="Valentin Gassend" />
-                <meta name="robots" content="index, follow" />
+                <meta charSet="UTF-8"/>
+                <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+                <meta name="author" content="Valentin Gassend"/>
+                <meta name="robots" content="index, follow"/>
                 <title>DEVELOPPEUR CREATIF - VALENTIN GASSEND</title>
-                <meta name="description" content="Je m'appelle Valentin Gassend et voici mon portfolio. Je suis un développeur front-end créatif disponible en freelance" />
-                <link rel="canonical" href="https://valentingassend.com" />
+                <meta name="description"
+                      content="Je m'appelle Valentin Gassend et voici mon portfolio. Je suis un développeur front-end créatif disponible en freelance"/>
+                <link rel="canonical" href="https://valentingassend.com"/>
             </Helmet>
 
             <BrowserRouter>
-                <Routes>
-                    <Route path={"/"} element={<Home/>}/>
-                    <Route path={"/projects"} element={<Projects/>}/>
-                    <Route path={"/about"} element={<AboutPage/>}/>
-                    <Route path={"/project/:id"} element={<SingleProjectPage/>}/>
-                </Routes>
+                <Suspense fallback={<LoadingSpinner/>}>
+
+                    <Routes>
+                        <Route path={"/"} element={<Home/>}/>
+                        <Route path={"/projects"} element={<Projects/>}/>
+                        <Route path={"/about"} element={<AboutPage/>}/>
+                        <Route path={"/project/:id"} element={<SingleProjectPage/>}/>
+                    </Routes>
+                </Suspense>
             </BrowserRouter>
         </>
     )
--- vite.config.js
+++ vite.config.js
@@ -1,65 +1,73 @@
-import { defineConfig } from 'vite';
+import {defineConfig} from 'vite';
 import react from '@vitejs/plugin-react';
 import viteImagemin from 'vite-plugin-imagemin';
 import generateSitemap from "./generateSitemap.js";
 
 generateSitemap();
 export default defineConfig({
-  plugins: [
-    react(),
-    viteImagemin({
-      gifsicle: {
-        optimizationLevel: 7,
-        interlaced: false,
-      },
-      optipng: {
-        optimizationLevel: 7,
-      },
-      mozjpeg: {
-        quality: 80,
-      },
-      pngquant: {
-        quality: [0.65, 0.9],
-        speed: 4,
-      },
-      svgo: {
-        plugins: [
-          {
-            name: 'removeViewBox',
-          },
-          {
-            name: 'removeEmptyAttrs',
-            active: false,
-          },
-        ],
-      },
-      webp: {
-        quality: 80,
-      },
-    })
-  ],
-  build: {
-    // Enable CSS code splitting
-    cssCodeSplit: true,
-    // Enable brotli compression for static files
-    brotliSize: true,
-    rollupOptions: {
-      output: {
-        // Customize the asset file names
-        assetFileNames: (assetInfo) => {
-          const extType = assetInfo.name.split('.').pop();
-          if (extType === 'css') {
-            return 'assets/css/[name].[hash][extname]';
-          } else if (extType === 'js') {
-            return 'assets/js/[name].[hash][extname]';
-          } else {
-            return 'assets/[name].[hash][extname]';
-          }
-        },
-        // Use long-term caching for assets
-        chunkFileNames: 'assets/js/[name].[hash].js',
-        entryFileNames: 'assets/js/[name].[hash].js'
-      }
+    plugins: [
+        react(),
+        viteImagemin({
+            gifsicle: {
+                optimizationLevel: 7,
+                interlaced: false,
+            },
+            optipng: {
+                optimizationLevel: 7,
+            },
+            mozjpeg: {
+                quality: 80,
+            },
+            pngquant: {
+                quality: [0.65, 0.9],
+                speed: 4,
+            },
+            svgo: {
+                plugins: [
+                    {
+                        name: 'removeViewBox',
+                    },
+                    {
+                        name: 'removeEmptyAttrs',
+                        active: false,
+                    },
+                ],
+            },
+            webp: {
+                quality: 80,
+            },
+        })
+    ],
+    build: {
+        sourcemap: false, // Désactivez les sourcemaps en production
+        chunkSizeWarningLimit: 1000,
+        // Enable CSS code splitting
+        cssCodeSplit: true,
+        // Enable brotli compression for static files
+        brotliSize: true,
+        rollupOptions: {
+            output: {
+                manualChunks: {
+                    vendor: ['react', 'react-dom', 'react-router-dom'],
+                    gsap: ['gsap', 'gsap/ScrollTrigger', 'gsap/ScrollSmoother'],
+                    three: ['three'],
+                    utilities: ['lenis', 'swiper']
+                },
+                // Customize the asset file names
+                assetFileNames: (assetInfo) => {
+                    const extType = assetInfo.name.split('.').pop();
+                    if (extType === 'css') {
+                        return 'assets/css/[name].[hash][extname]';
+                    } else if (extType === 'js') {
+                        return 'assets/js/[name].[hash][extname]';
+                    } else {
+                        return 'assets/[name].[hash][extname]';
+                    }
+                },
+                // Use long-term caching for assets
+                chunkFileNames: 'assets/js/[name].[hash].js',
+                entryFileNames: 'assets/js/[name].[hash].js'
+            }
+        }
     }
-  }
 });
